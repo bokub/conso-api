614856041f03d27fdddcf87937e798408f4ce784
Boris Kubiak <kubiakboris@gmail.com>
2023-05-09 09:04:00 +0200

    Fix mobile header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="container mx-auto max-w-4xl sm:pt-6 sm:pb-10">
-    <div class="flex justify-between wrapper px-4 py-4 sm:px-8 relative">
-      <!-- Navigation -->
+  <div class="container mx-auto max-w-4xl sm:pb-10">
+    <div class="flex flex-col sm:flex-row justify-between px-4 py-4 sm:px-8 gap-y-3 relative sm:items-end">
+      <!-- Logo-->
       <NuxtLink to="/">
-        <div class="logo absolute bottom-[20px] flex flex-row gap-2">
+        <div class="logo flex flex-row gap-2">
           <img src="/zap.svg" width="20" alt="Conso API Logo" />
           <div>Conso API</div>
         </div>
       </NuxtLink>
+      <!-- Navigation -->
       <div>
         <NuxtLink to="/" active-class="font-bold" class="mr-6">Accueil</NuxtLink>
         <NuxtLink to="/exemples" active-class="font-bold" class="mr-6">Exemples</NuxtLink>
