0e35495c4633605a9404bbe77d55d4b90c55cac7
Boris Kubiak <kubiakboris@gmail.com>
2023-10-05 15:24:05 +0200

    Improve ban script

--- .github/scripts/ipban.mjs
+++ .github/scripts/ipban.mjs
@@ -1,10 +1,17 @@
 #!/usr/bin/env zx
+$.verbose = false;
 
 const BAN_THRESHOLD = 100;
 const CHECK_INTERVAL_HOURS = 12;
 
 const from = new Date();
 from.setHours(from.getHours() - CHECK_INTERVAL_HOURS);
+
+function anonymize(ip) {
+  const separator = ip.includes(':') ? ':' : '.';
+  const parts = ip.split(separator);
+  return [parts[0], parts[1].replace(/./g, '•'), parts[2].replace(/./g, '•'), parts[3]].join(separator);
+}
 
 const logsResponse = await fetch(
   'https://logs.betterstack.com/api/v1/query?' +
@@ -18,9 +25,14 @@ const logsResponse = await fetch(
       Authorization: 'Bearer ' + process.env.LOGS_API_TOKEN,
     },
   }
-);
+).catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
 
 const logsBody = await logsResponse.json();
+
+console.info(`Processing ${logsBody.data.length} logs...`);
 
 const ipMap = {};
 for (const log of logsBody.data) {
@@ -41,7 +53,10 @@ if (bans.length === 0) {
   process.exit(0);
 }
 
-console.info(`Found ${bans.length} IP(s) to ban`);
+console.info(`\nFound ${bans.length} IP(s) to ban:`);
+for (const ban of bans) {
+  console.info(`${anonymize(ban.ip)} - ${ban.comment}`);
+}
 
 const cloudflareURL = `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT}/rules/lists/${process.env.BLACKLIST_ID}/items`;
 const banResponse = await fetch(cloudflareURL, {
@@ -50,5 +65,10 @@ const banResponse = await fetch(cloudflareURL, {
   headers: { 'Content-Type': 'application/json', Authorization: 'Bearer ' + process.env.CLOUDFLARE_TOKEN },
 });
 
-console.info('Ban result:');
-console.info(await banResponse.json());
+if (banResponse.status !== 200) {
+  console.error('\nBan error:');
+  console.error(await banResponse.json());
+  process.exit(1);
+}
+
+console.info('\nBan successful!');
